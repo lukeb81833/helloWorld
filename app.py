f1316b49af394f7dfbb03df7f4a1ce53df47c19f
Luke Bennett <124008861+lukeb81833@users.noreply.github.com>
2023-03-27 21:29:52 -0400

    Did 1-5

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,17 @@ def hello():  # put application's code here
 @app.route('/about')
 def about():  # put application's code here
     return render_template('about.html')
+
+@app.route('/favorite-course')
+def favoritecourse():  # put application's code here
+    print('Course Entered: ' + request.args.get('course_name'))
+    print('Course Number Entered: ' + request.args.get('course_number'))
+
+    return render_template('favorite-course.html')
+
+@app.route('/contact')
+def contact():  # put application's code here
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
